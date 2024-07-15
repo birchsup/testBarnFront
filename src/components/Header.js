c5bffa7703d4d51813e7608863895318a34740bb
DmitrySado <168556229+DmitrySado@users.noreply.github.com>
2024-07-16 02:34:59 +0400

    second commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Header.css';
-import logo from '../assets/logo.png'; // Убедитесь, что путь к логотипу правильный
+import logo from '../assets/logo.webp'; // Убедитесь, что путь к логотипу правильный
 
 const Header = () => {
     const navigate = useNavigate();
 
     return (
         <header className="app-header">
-            <div className="header-logo" onClick={() => navigate('/')}>
-                <img src={logo} alt="TestBarn Logo" className="logo-image" />
-                TestBarn
+            <div className="header-left">
+                <div className="header-logo" onClick={() => navigate('/')}>
+                    <img src={logo} alt="TestBarn Logo" className="logo-image"/>
+                    TestBarn
+                </div>
+                <nav className="header-nav">
+                    <button className="header-button" onClick={() => navigate('/testcases')}>All Tests</button>
+                    <button className="header-button" onClick={() => navigate('/create')}>Create Test Case</button>
+                    <button className="header-button" onClick={() => navigate('/createTestSuite')}>Create Test Suite
+                    </button>
+                    <button className="header-button" onClick={() => navigate('/createTestRun')}>Create Test Run
+                    </button>
+                </nav>
             </div>
-            <nav className="header-nav">
-                <button className="header-button" onClick={() => navigate('/testcases')}>All Tests</button>
-                <button className="header-button" onClick={() => navigate('/create')}>Create Test Case</button>
-            </nav>
         </header>
     );
 };
--- src/home/Home.js
+++ src/home/Home.js
@@ -5,11 +5,6 @@ const Home = () => {
 
     return (
         <div className="home">
-            {/*<h1>Welcome to TestBarn</h1>*/}
-            {/*<div className="home-buttons">*/}
-            {/*    <button className="header-button" onClick={handleAllTestsClick}>All Tests</button>*/}
-            {/*    <button className="header-button">TestPlan</button>*/}
-            {/*</div>*/}
         </div>
     );
 };
